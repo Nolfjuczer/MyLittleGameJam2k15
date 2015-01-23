ddc9dca0fd7ee2af20247e01b31245fabbc981c6
Filip Wróbel <wrobelfilip@gmail.com>
2015-01-23 22:11:09 +0100

    [+] Minigame base class [+] Bar mashing Minigame [+] Minigame manager [+] Minigame test manager

---
+++ Assets/Scripts/Minigame.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class Minigame : MonoBehaviour 
+{
+    #region Variables
+    #endregion
+
+    #region Events
+
+    public Action OnMinigameWin;
+    public Action OnMinigameLost;
+
+    #endregion
+
+
+    #region Monobehaviour Variables
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    void OnEnable()
+    {
+        this.OnMinigameWin += SelfDisable;
+    }
+    void OnDisable()
+    {
+        this.OnMinigameWin -= SelfDisable;
+    }
+    #endregion
+
+    #region Methods
+
+    public void NotifyOnMinigameWin()
+    {
+        if (this.OnMinigameWin != null)
+        {
+            this.OnMinigameWin();
+        }
+    }
+    public void NotifyOnMinigameLost()
+    {
+        if (this.OnMinigameLost != null)
+        {
+            this.OnMinigameLost();
+        }
+    }
+
+    protected void SelfDisable()
+    {
+        this.OnMinigameWin = null;
+        this.OnMinigameLost = null;
+        this.gameObject.SetActive(false);
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/MinigameTestManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinigameTestManager : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    void Start () 
+    {
+       
+	}
+	void Update () 
+    {
+	
+	}
+
+    void OnEnable()
+    {
+        MinigameManager.OnMinigameWin += Win;
+        MinigameManager.OnMinigameLost += Lost;
+    }
+    void OnDisable()
+    {
+        MinigameManager.OnMinigameWin -= Win;
+        MinigameManager.OnMinigameLost -= Lost;
+    }
+
+    void Win()
+    {
+        Debug.Log("yey won");
+    }
+    void Lost()
+    {
+        Debug.Log("you suck");
+    }
+    public void LaunchMinigame()
+    {
+        MinigameManager.LaunchMinigame();
+    }
+}
