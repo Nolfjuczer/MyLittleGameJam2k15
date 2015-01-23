4c1d606c9e2d65455cba821db57e3e861be50b22
Patryk Kochański <patryk.kochanski@hotmail.com>
2015-01-23 23:05:55 +0100

    Adding random generating doors :) and fixed lvl  :D now it shouldnt bug ^^

---
+++ Assets/Scripts/DoorSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorSpawner : MonoBehaviour {
+
+
+    public Sprite Doors;
+
+    public GameObject[] doors1 = new GameObject[2];
+    public GameObject[] doors2 = new GameObject[2];
+    public GameObject[] doors3 = new GameObject[2];
+
+	// Use this for initialization
+	void Start () {
+        
+        int rand = Random.Range(0, 1);
+        doors1[rand].gameObject.GetComponent<SpriteRenderer>().sprite = Doors;
+        rand = Random.Range(0, 1);
+        doors2[rand].gameObject.GetComponent<SpriteRenderer>().sprite = Doors;
+        rand = Random.Range(0, 1);
+        doors3[rand].gameObject.GetComponent<SpriteRenderer>().sprite = Doors;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
