fbca927985814d7ca22959acf0a892b5e14e75c1
Patryk Kochański <patryk.kochanski@hotmail.com>
2015-01-23 21:35:22 +0100

    Adding player controller script, base lvl, made some changes in InputManager, added some graphics, GUI controller, GUI elements and GameController

---
+++ Assets/Scripts/GUIController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIController : MonoBehaviour {
+
+    public Text Timer;
+    public Text FixedItems;
+
+    private int secs;
+    private int mins;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        secs = (int)Time.time % 60;
+        mins = (int)Time.time / 60;
+        Timer.text = string.Format("{0:00}:{1:00}", mins, secs);
+        FixedItems.text = "Fixed: " + GameController.Instance.FixedItems;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetAxis("Vertical")>0.0f)
+        {
+            this.transform.position += new Vector3(0.0f, 0.05f, 0.0f);
+        }
+        if(Input.GetAxis("Vertical")<0.0f)
+        {
+            this.transform.position += new Vector3(0.0f, -0.05f, 0.0f);
+        }
+        if (Input.GetAxis("Horizontal") > 0.0f)
+        {
+            this.transform.position += new Vector3(0.05f, 0.0f, 0.0f);
+        }
+        if (Input.GetAxis("Horizontal") < 0.0f)
+        {
+            this.transform.position += new Vector3(-0.05f, 0.0f, 0.0f);
+        }
+
+	
+	}
+}
